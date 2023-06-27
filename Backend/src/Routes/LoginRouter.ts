0b6a30c4002b210c3fbac625a65bd79bb55ab67b
Aleksandr <ryan00@mail.ru>
2023-06-27 18:43:25 +0300

    d

--- Backend/src/Routes/LoginRouter.ts
+++ Backend/src/Routes/LoginRouter.ts
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-const validationLogin = require("../../dist/ValidationShema/ValidationLogin.js");
+const validationLogin = require("../../dist/ValidationShema/validationLogin.js");
 let { LoginController } = require("../Controllers/LoginController");
 router.route("/").post(validationLogin, LoginController);
 module.exports = router;
