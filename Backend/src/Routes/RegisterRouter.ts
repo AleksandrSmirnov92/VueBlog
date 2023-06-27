c321b87c76e92699562e006609eda86e1f98f903
Aleksandr <ryan00@mail.ru>
2023-06-27 18:40:08 +0300

    deploy

--- Backend/src/Routes/RegisterRouter.ts
+++ Backend/src/Routes/RegisterRouter.ts
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-const validationRegister = require("../../dist/ValidationShema/ValidationRegister.js");
+const validationRegister = require("../../dist/ValidationShema/validationRegister.js");
 let { RegisterController } = require("../Controllers/RegisterController");
 router.route("/").post(validationRegister, RegisterController);
 module.exports = router;
