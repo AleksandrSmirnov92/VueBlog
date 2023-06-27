fbb5bf64e8d88c94c617a288af212159dbf8d226
Aleksandr <ryan00@mail.ru>
2023-06-27 20:21:35 +0300

    d

--- Frontend/src/axios.ts
+++ Frontend/src/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 const user = JSON.parse(window.localStorage.getItem("user")!);
-axios.defaults.baseURL = "http://localhost:9999";
+
+// axios.defaults.baseURL = "http://localhost:9999";
+axios.defaults.baseURL = "https://vueblog-332d.onrender.com";
 if (user) {
   axios.defaults.headers.common["Authorization"] = "Bearer " + user.token;
 }
